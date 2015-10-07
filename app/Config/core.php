acea57bd9117a30e9d3e0dd97127633ec8232157
ogran-org <ogran.n@gmail.com>
2015-10-07 22:15:10 +0900

    core.php salt変更

--- app/Config/core.php
+++ app/Config/core.php
@@ -221,12 +221,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'UWpTHqLmWCEpL6brPONnpD3ywzIIkcnsBPQHIb7a');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '74101653698016823764393752432');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
